c3a35caa76b7ab0015aeb079437b440a8e198188
Kiatsko <k.a.iatsko@gmail.com>
2021-01-07 17:55:04 +0300

    Adjust Pan

--- app/src/main/java/com/knstntn/mycalc/MainActivity.kt
+++ app/src/main/java/com/knstntn/mycalc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.knstntn.mycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-enum class Operation {
-    PLUS, MINUS, MULTIPLE, DIVIDE
+enum class Operation (
+    var label: String,
+    var calc: (Double, Double) -> Double
+        ) {
+    PLUS("+", Double::plus),
+    MINUS("-", Double::minus),
+    MULTIPLE("x", Double::times),
+    DIVIDE("/", Double::div)
 }
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         var operation: Operation? = null
         var number: Double? = null
 
-        val txView = findViewById(R.id.textView)
+        val txView = findViewById<TextView>(R.id.textView)
         val editTxt = findViewById<TextView>(R.id.editTextNumberDecimal)
         val plus = findViewById<TextView>(R.id.buttonPlus)
         val minus = findViewById<TextView>(R.id.buttonMinus)
@@ -25,14 +33,32 @@ class MainActivity : AppCompatActivity() {
         val clean = findViewById<TextView>(R.id.buttonClean)
         val equals = findViewById<TextView>(R.id.buttonEquals)
 
-        val function
+
+        txView.setOnLongClickListener {
+            return@setOnLongClickListener false
+        }
+        val function = View.OnClickListener{view ->
+            var nexnumber = textView.text.toString().toDoubleOrNull() ?: return@OnClickListener
+            val nexoperation = when (view.id) {
+                R.id.buttonPlus -> Operation.PLUS
+                R.id.buttonMinus -> Operation.MINUS
+                R.id.buttonMult -> Operation.MULTIPLE
+                R.id.buttonDiv -> Operation.DIVIDE
+
+                else -> null
+            }
+
+
+
+        }
 
         clean.setOnClickListener{
-            txView = ""
-            editTxt = ""
+            txView.text = ""
+            editTxt.text = ""
             number = null
             operation = null
         }
+
 
     }
 }
